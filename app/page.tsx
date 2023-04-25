91c61134497ad77cd69e0ee2a4aae37543393a99
Mohamed Atef <atefhamosa@gmail.com>
2023-04-25 08:14:32 +0200

    Add ChatGPT

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from 'next/navigation';
 import { Github, Linkedin, Mail } from "lucide-react";
 import { Card } from "./components/card";
 import Link from "next/link";
 import Image from "next/image";
+import { Configuration, OpenAIApi } from 'openai';
 
-const navigation = [
-	{ name: "Projects", href: "/projects" },
-	{ name: "Contact", href: "/contact" },
-];
+const configuration = new Configuration({
+	apiKey: process.env.OPEN_API,
+  });
+const openai = new OpenAIApi(configuration);
+const getResponse = async (prompt: string) => {
+	const response = await openai.createCompletion({
+	model: "text-davinci-003",
+	prompt: `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with \"I will not answer that :'(\".\n\nQ: What is human life expectancy in the United States?\nA: Human life expectancy in the United States is 78 years.\n\nQ: Who was president of the United States in 1955?\nA: Dwight D. Eisenhower was president of the United States in 1955.\n\nQ: Which party did he belong to?\nA: He belonged to the Republican Party.\n\nQ: What is the square root of banana?\nA: I will not answer that :'(\n\nQ: How does a telescope work?\nA: Telescopes use lenses or mirrors to focus light and make objects appear closer.\n\nQ: Where were the 1992 Olympics held?\nA: The 1992 Olympics were held in Barcelona, Spain.\n\nQ: How many squigs are in a bonk?\nA: I will not answer that :'(\n\nQ: ${prompt}?\nA:`,
+	  temperature: 0,
+	  max_tokens: 100,
+	  top_p: 1,
+	  frequency_penalty: 0.0,
+	  presence_penalty: 0.0,
+	  stop: ["\n"],
+	});
+	return response;
+  }
 
 const socials = [
 	{
@@ -34,7 +48,13 @@ const socials = [
 ];
 
 export default function Home() {
+	const [answer, setAnswer] = useState<string | undefined>();
+	const [asking, setAsking] = useState(false);
+	const [open, setOpen] = useState(false);
+	const [prompt, setPrompt] = useState('');
+	const handleOpen = () => setOpen(!open);
 	const router = useRouter()
+
 	return (
 		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden p-3">
 			<div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0 w-full h-full fixed"></div>
@@ -79,8 +99,63 @@ export default function Home() {
 								setTimeout(()=>{
 									router.push("/projects");
 								}, 900)
-								
 							}}>My Projects</button>
+							<button className="text-sky-400 outline outline-1 outline-sky-400a text-sm md:text-lg bg-blue rounded-xl p-3 px-8 mt-5 tracking-wider ml-2 font-thin"
+							onClick={()=>{
+								handleOpen();
+							}}>Ask GPT</button>
+							{open && <div className="dialog-container flex fixed w-full h-full bg-gray-800 bg-opacity-50 items-center justify-center top-0 left-0">
+								<div className="dialog bg-white p-4 rounded-2xl w-1/2 max-w-xl relative">
+									<button className="close absolute top-0 right-0 text-gray-500 m-4 border px-2 rounded"
+									onClick={()=>{
+										setAnswer(undefined);
+										setOpen(false);
+										setPrompt('');
+									}}
+									>×</button>
+									<h2 className="text-center text-black mb-3 text-3xl">GPT CHAT</h2>
+									<h3 className=" text-gray-900 font-sans font-thin">A chance to talk to GPT while you are here...</h3>
+									
+									{answer? <div className="answer bg-black text-white font-thin font-mono p-3 rounded-lg mt-3"><div className=" text-green-600 mb-2">Q: {prompt}?</div> A: {answer}</div>: <textarea onChange={(e)=> setPrompt(e.target.value)} className="border border-1 border-gray-300 selection:border-grey-400 w-full p-2 mt-2 rounded" placeholder="How are you doing GPT?" rows={4} aria-expanded={false} autoFocus autoCorrect="false"></textarea>}
+									{!answer && <button onClick={async ()=>{
+										setAsking(true)
+										await getResponse(prompt).then(res => {
+											const ans = res.data.choices[0].text?.trim()
+											for (let i = 0; i < ans.length; i++) {
+												setTimeout(()=>{
+													setAnswer(prev => {
+														if (prev) {
+															return prev + ans[i]
+														} else {
+															return ans[i]
+														}
+													})
+												}, (i*20) + 200)
+											}
+										});
+										setAsking(false)
+									}} className="p-2 w-full rounded text-white" style={{
+										backgroundColor: asking ? "gray":"black"
+									}}>{asking ? "LOADING...":"ASK"}</button>}
+									{answer && <div className="flex justify-between">
+										<button onClick={async ()=>{
+												setAnswer(undefined);
+												setPrompt('');
+											}} className="p-2 rounded px-4 text-white mt-3" style={{
+												backgroundColor: asking ? "gray":"black"
+											}}>ASK AGAIN</button>
+
+										<button onClick={async ()=>{
+												setAnswer(undefined);
+												setOpen(false);
+												setPrompt('');
+											}} className="p-2 rounded px-4 text-white mt-3" style={{
+												backgroundColor: asking ? "gray":"black"
+											}}>Thanks Atef..!</button>
+									</div>}
+								</div>
+							</div>}
+							
 							{/* <button className="text-sky-400 outline outline-1 outline-sky-400a text-sm md:text-lg bg-blue rounded-xl p-3 px-8 mt-5 tracking-wider ml-2 font-thin"
 							onClick={(e)=>{
 								document.querySelectorAll(".fades-left").forEach((el)=> {el.classList.add("fade-left")});
--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,14 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		appDir: true,
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    appDir: true,
+    mdxRs: true,
+  },
+  env: {
+    OPEN_API: "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",
+  },
 };
 
 export default withContentlayer(nextConfig);
